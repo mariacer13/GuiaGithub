0bf7cd097507992a96348bd322815727bd094a3d
Mariana Santiago <issavalecl@.gmail.com>
2023-09-12 23:45:47 -0500

    creacion proyecto

---
+++ src/main/java/com/trabajo/guiagithub/GuiagithubApplication.java
@@ -0,0 +1,13 @@
+package com.trabajo.guiagithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiagithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiagithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabajo/guiagithub/GuiagithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajo.guiagithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiagithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
